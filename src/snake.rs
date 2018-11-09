e7216addef127f8bf07ea54aed431f567c4f73e6
streletss <xstreletssx@gmail.com>
2018-11-10 01:39:58 +0200

    move color and add methods

--- src/snake.rs
+++ src/snake.rs
@@ -1,12 +1,10 @@
 use std::collections::LinkedList;
 use piston_window::{Context, G2d};
-use piston_window::types::Color;
 
 use draw::draw_block;
+use colors::SNAKE_COLOR;
 
-const SNAKE_COLOR: Color = [0.00, 0.80, 0.00, 1.00];
-
-#[derive(Copy, Clone)]
+#[derive(Copy, Clone, PartialEq)]
 pub enum Direction {
   Up,
   Down,
@@ -71,6 +69,21 @@ impl Snake {
     self.direction
   }
 
+  pub fn head_next(&self, dir: Option<Direction>) -> (i32, i32) {
+    let (head_x, head_y): (i32, i32) = self.head_pos();
+    let move_dir = match dir {
+      Some(d) => d,
+      None => self.direction,
+    };
+
+    match move_dir {
+      Direction::Up => (head_x, head_y - 1),
+      Direction::Down => (head_x, head_y + 1),
+      Direction::Left => (head_x - 1, head_y),
+      Direction::Right => (head_x + 1, head_y),
+    }
+  }
+
   pub fn move_forward(&mut self, dir: Option<Direction>) {
     match dir {
       None => (),
@@ -99,5 +112,42 @@ impl Snake {
     };
     self.body.push_front(new_block);
     self.tail = Some(removed_block);
+  }
+
+  pub fn tail_restore(&mut self) {
+    let tail_block = self.tail.clone().unwrap();
+    self.body.push_back(tail_block);
+  }
+
+  pub fn tail_overlaps(&self, x: i32, y: i32) -> bool {
+    let mut ch = 0;
+    for block in &self.body {
+      if block.x == x && block.y == y {
+        return true;
+      }
+      ch += 1;
+      if ch == self.body.len() - 1 {
+        break;
+      }
+    }
+    false
+  }
+
+  pub fn check_if_alive(
+    &self,
+    dir: Option<Direction>,
+    game_width: i32,
+    game_height: i32,
+  ) -> bool {
+    let (next_x, next_y) = self.head_next(dir);
+
+    if self.tail_overlaps(next_x, next_y) {
+      return false;
+    }
+
+    next_x > 0
+      && next_y > 0
+      && next_x < game_width - 1
+      && next_y < game_height - 1
   }
 }
