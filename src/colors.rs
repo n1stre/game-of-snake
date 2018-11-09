8de8022c789f01af6f0bd2418597c8cc8abd0d10
streletss <xstreletssx@gmail.com>
2018-11-10 01:36:42 +0200

    add colors

---
+++ src/colors.rs
@@ -0,0 +1,7 @@
+use piston_window::types::Color;
+
+pub const SNAKE_COLOR: Color = [0.00, 0.80, 0.00, 1.0];
+pub const FOOD_COLOR: Color = [0.80, 0.00, 0.00, 1.0];
+pub const BORDER_COLOR: Color = [0.00, 0.00, 0.00, 1.0];
+pub const GAMEOVER_COLOR: Color = [0.90, 0.00, 0.00, 0.5];
+pub const BG_COLOR: Color = [0.5, 0.5, 0.5, 1.0];
